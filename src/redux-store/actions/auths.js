11dc83c058ccfb040cd596bd4f10fe3e63c44977
david kuseh <davegru203@gmail.com>
2021-05-18 19:15:34 +0000

    make changes to items in localStorage

--- src/redux-store/actions/auths.js
+++ src/redux-store/actions/auths.js
@@ -10,7 +10,7 @@ export const register = (credentials, history) => dispatch => {
           localStorage.setItem("firstname", res.data.user.firstName);
           localStorage.setItem("lastname", res.data.user.lastName);
           localStorage.setItem("token", res.data.token);
-          localStorage.setItem("userID", res.data.user.id);
+          localStorage.setItem("userId", res.data.user.id);
           dispatch({ type: types.REGISTER_SUCCESS, payload: res.data });
           history.push("/login");
       })
@@ -26,10 +26,12 @@ export const login = (credentials, history) => dispatch => {
     Axios()
       .post(`${process.env.REACT_APP_API_BASE_URL}/api/auth/login`, credentials)
       .then(res => {
-        localStorage.setItem("firstname", res.data.firstName);
-        localStorage.setItem("lastname", res.data.lastName);
+        console.log(res.data)
+        localStorage.setItem("userId", res.data.user.id);
+        localStorage.setItem("firstname", res.data.user.firstName);
+        localStorage.setItem("lastname", res.data.user.lastName);
         localStorage.setItem("token", res.data.token);
-        localStorage.setItem("userID", res.data.id);
+        localStorage.setItem("email", res.data.user.email);
         dispatch({ type: types.LOGIN_SUCCESS, payload: res.data });
         history.push("/");
       })
@@ -38,4 +40,7 @@ export const login = (credentials, history) => dispatch => {
           dispatch({ type: types.LOGIN_FAILURE, payload: error.message });
       });
 };
+
+
+
 
